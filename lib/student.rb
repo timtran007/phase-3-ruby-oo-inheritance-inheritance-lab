7a694d8bae1fca1bffa2c01a9bd163ee2e4d73d5
Tim <timtran007@gmail.com>
2022-11-27 22:34:14 -0600

    commit tree 15ef26215def0c6f6069d44aa43478a65be287d3

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,16 @@
-class Student 
+require_relative './user'
+require 'pry'
+class Student < User
+    attr_accessor :knowledge
 
+    def initialize (knowledge = [])
+        @knowledge = knowledge
+    end
+
+    def learn (string)
+        self.knowledge << string
+    end
 end
+
+sophie = Student.new
+binding.pry
---
+++ lib/user.rb
@@ -0,0 +1,9 @@
+class User
+    attr_accessor :first_name, :last_name
+
+    KNOWLEDGE = []
+
+    def initialize
+        @KNOWLEDGE = KNOWLEDGE
+    end
+end
